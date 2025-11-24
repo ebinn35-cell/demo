c35b7e307cf11fc817524903c69e56869b1f23c6
ebinn35-cell <ebinn35@gmail.com>
2025-11-24 15:57:31 +0530

    styled

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const data = [
-    {name : "abu" , email:"abu@gmail.com" , age : 22 , Gender:"male"},
-    {name : "fou" , email:"fou@gmail.com" , age : 23 , Gender:"male"},
-    {name : "abi" , email:"abi@gmail.com" , age : 18 , Gender:"male"},
-    {name : "kiyu" ,email:"kiyu@gmail.com", age : 55 , Gender:"female"},
-    {name : "gofu" ,email:"gofu@gmail.com", age : 19 , Gender:"male"},
-    {name : "maki" ,email:"maki@gmail.com", age : 20 , Gender:"female"},
+    {name : "abu" , email:"abu@gmail.com" ,age : 22 , Gender:"male"},
+    {name : "fou" , email:"fou@gmail.com" ,age : 23 , Gender:"male"},
+    {name : "abi" , email:"abi@gmail.com" ,age : 18 , Gender:"male"},
+    {name : "kiyu" , email:"kiyu@gmail.com" ,age : 55 , Gender:"female"},
+    {name : "gofu" , email:"gofu@gmail.com" ,age : 19 , Gender:"male"},
+    {name : "maki" , email:"maki@gmail.com" ,age : 20 , Gender:"female"},
 ]
 
 const tbody = document.getElementById('tbody')
@@ -12,6 +12,7 @@ const tbody = document.getElementById('tbody')
 data.forEach(a=>{
     const tr = document.createElement("tr")
     tr.innerHTML=`
+    <td style="text-align: center;">${a.no}</td>
     <td>${a.name}</td>
     <td>${a.email}</td>
     <td>${a.age}</td>
